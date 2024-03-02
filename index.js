caeb9b5e049cca1ed40ec15a6bf360ded87b6459
arvin0099 <arvincxzc@gmail.com>
2024-03-02 13:45:09 -0800

    market place

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const exp = require('constants')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
+const items = require('./models/items.js')
 const methodOverride = require('method-override')
 
 app.use(express.static('public'))
@@ -15,6 +16,11 @@ app.get('/home', (req, res) => {
     })
 })
 
+app.get('/market', (req, res) => {
+    res.render('market.ejs', {
+        items: items
+    })
+})
 app.listen(port, () => {
     console.log('Server Running')
 })
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/*.ejs",
+  "./public/css/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
